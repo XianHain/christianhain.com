d98b08427062144a2ed4d0a925da527d1fef6fc3
Christian Hain <910645+XianHain@users.noreply.github.com>
2024-05-09 16:48:28 -0400

    chore: add version to calVer.json
    The original implementation of the `calVer.json` file did not account
    for non-css/non-js updates. There's no point in refreshing the static
    cache, but it's also odd to have a versioned release without a file
    change.

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,7 +1,8 @@
 const htmlmin = require('html-minifier');
+const marked = require('marked');
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode('year', () => new Date().getFullYear());
 
   eleventyConfig.addTransform('htmlmin', function (content) {
     return ((this.page.outputPath || '').endsWith('.html'))
@@ -13,6 +14,29 @@ module.exports = function(eleventyConfig) {
         })
       : content;
   });
+
+  eleventyConfig.addFilter('blogListTimestamp', (date) =>
+    new Date(date).toLocaleString('en-US', {
+      day: '2-digit',
+      year: 'numeric',
+      month: 'short',
+    })
+  );
+
+  eleventyConfig.addFilter('blogPostTimestamp', (date) =>
+    new Date(date).toLocaleString('en-US', {
+      day: 'numeric',
+      year: 'numeric',
+      month: 'long',
+    })
+  );
+
+  eleventyConfig.addFilter('markdown', async (content) =>
+    await marked.parse(content)
+      .replace('<meta data-xian="article-start" />', '<div data-theme="article">')
+      .replace('<meta data-xian="article-end" />',   '</div>')
+      // .replace(/<hr data-xian="remove" \/>/g, '')
+  );
 
   return {
     dir: {
